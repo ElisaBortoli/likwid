15407a2b34596524f3a7f4731c5a9e21c3a19e7c
ElisaBortoli <elisa.bortoli3@gmail.com>
2018-09-03 12:04:54 +0200

    adding GCC, bench and ext folders

---
+++ GCC/perfgroup.d
@@ -0,0 +1,7 @@
+GCC/perfgroup.o: src/perfgroup.c src/includes/error.h \
+ src/includes/likwid.h src/includes/bstrlib.h src/includes/perfgroup.h \
+ /home/elisa/Scrivania/MHPC/likwid/ext/lua/includes/lua.h \
+ /home/elisa/Scrivania/MHPC/likwid/ext/lua/includes/luaconf.h \
+ /home/elisa/Scrivania/MHPC/likwid/ext/lua/includes/lauxlib.h \
+ /home/elisa/Scrivania/MHPC/likwid/ext/lua/includes/lua.h \
+ /home/elisa/Scrivania/MHPC/likwid/ext/lua/includes/lualib.h
